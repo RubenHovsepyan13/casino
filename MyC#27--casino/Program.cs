38d294b02cd4f7676fa4c4e91f96c4994523098e
ruben1399@outlook.com <ruben1399@outlook.com>
2024-09-14 18:12:36 +0400

    initial commit

---
+++ MyC#27--casino/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyC_27__casino
+{
+    class Bet
+    {
+        public int Amount { get; set; }
+
+        public Bet(int amount)
+        {
+            Amount = amount;
+        }
+        public virtual bool IsWinningBet(int diceRoll)
+        {
+            return false;
+        }
+        public virtual int Payout()
+        {
+            return Amount * 2;
+        }
+    }
+    class NumberBet : Bet
+    {
+        public NumberBet(int amount, int number) : base(amount)
+        {
+            Number = number;
+        }
+        public int Number { get; set; }
+        public override bool IsWinningBet(int diceRoll)
+        {
+            return Number == diceRoll;
+        }
+        public override int Payout()
+        {
+            return Amount * 6;
+        }
+    }
+    class OddBet : Bet
+    {
+        public OddBet(int amount) : base(amount)
+        {
+        }
+        public override bool IsWinningBet(int diceRoll)
+        {
+            return diceRoll % 2 != 0;
+        }
+    }
+    class EvenBet : Bet
+    {
+        public EvenBet(int amount) : base(amount)
+        {
+        }
+        public override bool IsWinningBet(int diceRoll)
+        {
+            return diceRoll % 2 == 0;
+        }
+    }
+    class Casino
+    {
+        private int _balance;
+        public Casino(int initialBalance)
+        {
+            _balance = initialBalance;
+        }
+        public void PlaceBet(Bet bet)
+        {
+            if (bet.Amount > _balance)
+            {
+                Console.WriteLine("Anbavarar mijocner");
+                return;
+            }
+            _balance -= bet.Amount;
+            Random random = new Random();
+            int randomNumber = random.Next(1, 7);
+            if (bet.IsWinningBet(randomNumber))
+            {
+                Console.WriteLine($"Number: {randomNumber}");
+                Console.WriteLine("You Wine");
+                _balance += bet.Payout();
+            }
+            else
+            {
+                Console.WriteLine($"Number: {randomNumber}");
+                Console.WriteLine("You Lose");
+            }
+        }
+        public void ShowBalance()
+        {
+            Console.WriteLine($"Your balance: {_balance}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Casino balance = new Casino(1000);
+            int yntrutyun;
+            do
+            {
+                balance.ShowBalance();
+                Console.WriteLine("------------------------------------------------------");
+                Console.WriteLine("Yntri xaxy");
+                Console.WriteLine();
+                Console.WriteLine("1 : Zar");
+                Console.WriteLine();
+                Console.WriteLine("2 : zuyg");
+                Console.WriteLine();
+                Console.WriteLine("3 : kent");
+                Console.WriteLine();
+                Console.WriteLine("0 : Exit");
+                Bet bet = null;
+
+                yntrutyun = Convert.ToInt32(Console.ReadLine());
+                if (yntrutyun == 1)
+                {
+                    Console.WriteLine("yntri 1 - 6 mihat tiv");
+                    int tiv = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("stavket");
+                    int stavka = Convert.ToInt32(Console.ReadLine());
+                    bet = new NumberBet(stavka, tiv);
+                }
+                else if (yntrutyun == 2)
+                {
+                    Console.WriteLine("stavket");
+                    int stavka = Convert.ToInt32(Console.ReadLine());
+                    bet = new EvenBet(stavka);
+                }
+                else if (yntrutyun == 3)
+                {
+                    Console.WriteLine("stavket");
+                    int stavka = Convert.ToInt32(Console.ReadLine());
+                    bet  = new OddBet(stavka);
+                }
+                else
+                {
+                    Console.WriteLine("Mersi xaxi hamr");
+                }
+               balance.PlaceBet(bet);
+            }
+            while (yntrutyun != 0);
+        }
+    }
+}
---
+++ MyC#27--casino/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyC#27--casino")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyC#27--casino")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("66df783e-df99-44f3-8204-f3af5d753c20")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
